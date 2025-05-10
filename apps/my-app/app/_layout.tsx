925a13edd7c33f244e3793b1d8d2e266eeea19cf
FukeKazki <kazkichi0906@gmail.com>
2025-05-10 22:20:37 +0900

    feat: すっきり

--- apps/my-app/app/_layout.tsx
+++ apps/my-app/app/_layout.tsx
@@ -1,19 +1,12 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native"
 import { useFonts } from "expo-font"
 import { Stack } from "expo-router"
 import { StatusBar } from "expo-status-bar"
 import "react-native-reanimated"
 
-import { useColorScheme } from "@/hooks/useColorScheme"
 import { TamaguiProvider } from "tamagui"
 import config from "../tamagui.config"
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   })
@@ -25,13 +18,11 @@ export default function RootLayout() {
 
   return (
     <TamaguiProvider config={config}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
     </TamaguiProvider>
   )
 }
